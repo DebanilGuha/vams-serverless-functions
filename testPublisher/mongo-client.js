91b9bf52be715c0af5f84cab45572f72e053dd51
Debanil Guha <debanil@hutechsolutions.com>
2022-09-20 15:07:09 +0530

    Renamed

--- testPublisher/mongo-client.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// Import the dependency.
-const { MongoClient } = require('mongodb');
-// Export a module-scoped MongoClient promise. By doing this in a separate
-// module, the client can be shared across functions.
-const client = new MongoClient("mongodb+srv://debanil:d3b%400_db@cluster0.wia8y.mongodb.net/vams?retryWrites=true&w=majority",
-  { useNewUrlParser: true,  useUnifiedTopology: true });
-module.exports = client.connect(); 
